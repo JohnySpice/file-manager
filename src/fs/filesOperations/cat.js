bd7eb4a9d88ace501282ffa246ce7c89cc6e5911
JohnySpice <tyli008@mail.ru>
2022-06-10 12:48:16 +0300

    feat: rewrite cat function with stream

--- src/fs/filesOperations/cat.js
+++ src/fs/filesOperations/cat.js
@@ -1,7 +1,18 @@
-import {readFile} from 'fs/promises';
 import {cd} from '../navigation/cd.js';
+import {createReadStream} from 'fs';
 
 export async function cat(basePath, pathToFile) {
     const filePath = await cd(basePath, pathToFile);
-    return readFile(filePath, {encoding: 'utf8'});
+    const readStream = createReadStream(filePath);
+    return new Promise((resolve, reject) => {
+        readStream.on('data', data => {
+            console.log(data.toString());
+        })
+            .on('error', err => {
+                reject(err);
+            })
+            .on('end', () => {
+                resolve();
+            });
+    });
 }
