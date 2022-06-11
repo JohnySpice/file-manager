cfacff727fe3c7c905d632f2a8558da932367500
JohnySpice <tyli008@mail.ru>
2022-06-11 15:18:47 +0300

    fix: fix crash after wrong params

--- src/fs/filesOperations/cp.js
+++ src/fs/filesOperations/cp.js
@@ -4,14 +4,9 @@ import {pipeline} from 'stream/promises';
 import {basename, join} from 'path';
 
 export async function cp(basePath, sourceFilePath, destFilePath) {
-    const [resolvedSourcePath, destResolvedPath] = await Promise.all([
-        resolve(basePath, sourceFilePath, true),
-        resolve(basePath, join(destFilePath, basename(sourceFilePath)))
-    ]);
+    const resolvedSourcePath = await resolve(basePath, sourceFilePath, true);
+    const destResolvedPath = await resolve(basePath, join(destFilePath, basename(sourceFilePath)));
     const rs = createReadStream(resolvedSourcePath);
     const ws = createWriteStream(destResolvedPath);
-    return pipeline(
-        rs,
-        ws
-    );
+    return pipeline(rs, ws);
 }
