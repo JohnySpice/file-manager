7c382421d2b9d2dd5f980b6650fdb4e929db1aa7
JohnySpice <tyli008@mail.ru>
2022-06-11 16:52:51 +0300

    feat: implement adding file name from source path to archive path

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -2,12 +2,13 @@ import {createBrotliCompress} from 'zlib';
 import {pipeline} from 'stream/promises';
 import {createReadStream, createWriteStream} from 'fs';
 import {resolve} from '../fs/navigation/index.js';
+import {basename, sep} from 'path';
 
 export async function compress(basePath, sourcePath, destinationPath) {
-    const gzip = createBrotliCompress();
+    const compressor = createBrotliCompress();
     const resolvedSrcPath = await resolve(basePath, sourcePath, true);
-    const resolvedDestPath = await resolve(basePath, destinationPath);
-    const rs = await createReadStream(resolvedSrcPath);
+    const resolvedDestPath = await resolve(basePath, `${destinationPath}${sep}${basename(sourcePath)}.br`);
+    const rs = createReadStream(resolvedSrcPath);
     const ws = createWriteStream(resolvedDestPath);
-    return pipeline(rs, gzip, ws);
+    return pipeline(rs, compressor, ws);
 }
