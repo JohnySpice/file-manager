a9121be2f9c8c0edd2d2da9c8288a32b04c0a678
JohnySpice <tyli008@mail.ru>
2022-06-11 18:09:10 +0300

    feat: change adding empty file to stream api

--- src/fs/filesOperations/add.js
+++ src/fs/filesOperations/add.js
@@ -1,8 +1,8 @@
-import {open} from 'fs/promises';
 import {resolve} from '../navigation/index.js';
+import {createWriteStream} from 'fs';
 
 export async function add(path, fileName) {
     const correctPath = await resolve(path, fileName);
-    const fd = await open(correctPath, 'a');
-    return fd.close();
+    const ws = createWriteStream(correctPath);
+    return ws.close();
 }
