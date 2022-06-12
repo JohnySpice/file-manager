07411034f668df952964537d83f10b9e0de703f4
JohnySpice <tyli008@mail.ru>
2022-06-12 11:23:56 +0300

    fix: rewrite cd function to avoid moving in file

--- src/fs/navigation/cd.js
+++ src/fs/navigation/cd.js
@@ -1,5 +1,11 @@
 import {resolve} from './pathResolver.js';
+import {stat} from 'fs/promises';
 
 export async function cd(basePath, destPath) {
-    return resolve(basePath, destPath, true);
+    const resolvedDestPath = await resolve(basePath, destPath, true);
+    const pathStat = await stat(resolvedDestPath);
+    if (!pathStat.isDirectory()) {
+        throw new Error();
+    }
+    return resolvedDestPath;
 }
